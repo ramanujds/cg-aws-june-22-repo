27a7e00fd342951476b09524a32d47ee6f824641
Ramanuj DS <ramanujds9@gmail.com>
2022-06-20 17:05:48 +0530

    Model View Controller

--- spring-web-app.zip_expanded/spring-web-app/src/main/java/com/cg/springwebapp/controller/AppController.java
+++ spring-web-app.zip_expanded/spring-web-app/src/main/java/com/cg/springwebapp/controller/AppController.java
@@ -1,14 +1,19 @@
 package com.cg.springwebapp.controller;
 
+import java.time.LocalTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
 
 	@GetMapping("/hello")
-	public String sayHello() {
-		return "hello.html";
+	public String sayHello(Model m) {
+		LocalTime time = LocalTime.now();
+		m.addAttribute("time", time);
+		return "show.jsp";
 	}
 	
 }
---
+++ spring-web-app.zip_expanded/spring-web-app/src/main/webapp/show.jsp
@@ -0,0 +1,14 @@
+<%@page import="java.time.LocalDate"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" isELIgnored="false"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>Hello Sir..</h1>
+<h2>Current System Time : ${time} </h2>
+</body>
+</html>
