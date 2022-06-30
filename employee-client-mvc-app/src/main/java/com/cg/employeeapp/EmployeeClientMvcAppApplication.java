2bc75627d56773fb144c937033bb9835c11ced21
Ramanuj DS <ramanujds9@gmail.com>
2022-06-30 16:22:19 +0530

    RestTemplate

---
+++ employee-client-mvc-app/src/main/java/com/cg/employeeapp/EmployeeClientMvcAppApplication.java
@@ -0,0 +1,13 @@
+package com.cg.employeeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeClientMvcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeClientMvcAppApplication.class, args);
+	}
+
+}
---
+++ employee-client-mvc-app/src/test/java/com/cg/employeeapp/EmployeeClientMvcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.employeeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeClientMvcAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
